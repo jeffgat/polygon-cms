764e0556f7aa64dd5816bb2946bc0cdc8a323b5d
jeffgat <jeff.gtbn@gmail.com>
2022-09-04 15:35:42 -0700

    Init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 
+import "@ethersproject/abi";
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: '0.8.9',
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+    },
 };
 
 export default config;
